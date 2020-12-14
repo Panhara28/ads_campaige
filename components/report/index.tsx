783758a22570b04179dca9a1a88be9d732030965
Sambath Udomreachea <udomreacheasambath@gmail.com>
2020-12-14 14:54:23 +0700

    add_graph

--- components/report/index.tsx
+++ components/report/index.tsx
@@ -9,10 +9,106 @@ import {
   Table,
 } from "react-bootstrap";
 
+import { ResponsiveLine } from "@nivo/line";
+
+// install (please make sure versions match peerDependencies)
+// yarn add @nivo/core @nivo/line
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+
+interface GraphProps {
+  data: any;
+}
+
+const Graph = ({ data /* see data tab */ }) => (
+  <ResponsiveLine
+    data={data}
+    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+    xScale={{ type: "point" }}
+    yScale={{
+      type: "linear",
+      min: "auto",
+      max: "auto",
+      stacked: true,
+      reverse: false,
+    }}
+    yFormat=" >-.2f"
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      orient: "bottom",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "transportation",
+      legendOffset: 36,
+      legendPosition: "middle",
+    }}
+    axisLeft={{
+      orient: "left",
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: "count",
+      legendOffset: -40,
+      legendPosition: "middle",
+    }}
+    pointSize={10}
+    pointColor={{ theme: "background" }}
+    pointBorderWidth={2}
+    pointBorderColor={{ from: "serieColor" }}
+    pointLabelYOffset={-12}
+    useMesh={true}
+    legends={[
+      {
+        anchor: "bottom-right",
+        direction: "column",
+        justify: false,
+        translateX: 100,
+        translateY: 0,
+        itemsSpacing: 0,
+        itemDirection: "left-to-right",
+        itemWidth: 80,
+        itemHeight: 20,
+        itemOpacity: 0.75,
+        symbolSize: 12,
+        symbolShape: "circle",
+        symbolBorderColor: "rgba(0, 0, 0, .5)",
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemBackground: "rgba(0, 0, 0, .03)",
+              itemOpacity: 1,
+            },
+          },
+        ],
+      },
+    ]}
+  />
+);
+
 export const Overview: React.FC<any> = (): any => {
   const submitBtnHandler = (e: any) => {
     e.preventDefault();
   };
+
+  const data = [
+    {
+      id: "japan",
+      color: "hsl(220, 70%, 50%)",
+      data: [
+        {
+          x: "plane",
+          y: 145,
+        },
+      ],
+    },
+  ];
 
   return (
     <div className={styled.reportContainer}>
@@ -104,8 +200,12 @@ export const Overview: React.FC<any> = (): any => {
         </div>
       </div>
 
-      <div className={styled.chartContainer}>
-        <div className={styled.chart}></div>
+      <div className={styled.graphContainer}>
+        <div className={styled.chartContainer}>
+          <div className={styled.chart}>
+            <Graph data={data} />
+          </div>
+        </div>
       </div>
 
       <div className={styled.dataSetsContainer}>
